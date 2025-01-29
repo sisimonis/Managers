764f4aef13e85a3b077681122d314595f344962f
simon <simon.pit@hotmail.fr>
2025-01-30 00:10:39 +0100

    Scripts

---
+++ Runtime/Manager.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+using UnityEngine;
+
+public abstract class Manager<T> : MonoBehaviour where T : Manager<T>
+{
+    private static T instance;
+    public static T Instance { get { return instance; } }
+
+    protected virtual void Awake()
+    {
+        instance = (T)this;
+    }
+}
